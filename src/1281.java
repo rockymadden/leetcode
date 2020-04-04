e09ea810796d3256ce44a9fe530396ad33ed1021
Rocky Madden <github@rockymadden.com>
2020-04-04 18:22:13 +0000

    feat: create 1281 with Java

---
+++ src/1281.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int i, remainder, sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+            remainder = n % 10;
+            n = n / 10;
+            product *= remainder;
+            sum += remainder;
+        }
+        
+        return product - sum;
+    }
+}
