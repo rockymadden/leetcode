dcff079be8ec6fe8eaed6b47f86a3adbed63f7f4
Rocky Madden <github@rockymadden.com>
2020-04-14 12:57:53 +0000

    feat: resolve 202 with Java via tortoise and hare

--- src/202.java
+++ src/202.java
@@ -1,24 +1,25 @@
 class Solution {
     private int next(int n) {
         int sum = 0;
-
+        
         while (n > 0) {
-            int digit = n % 10;
+            int d = n % 10;
             n = n / 10;
-            sum += digit * digit; 
+            sum += d * d;
         }
         
         return sum;
     }
-
+    
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
+        int tortise = next(n);
+        int hare = next(next(n));
         
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = next(n);
+        while ((tortise != 1 || hare != 1) && tortise != hare) {
+            tortise = next(tortise);
+            hare = next(next(hare));
         }
         
-        return n == 1;
+        return tortise == 1 || hare == 1;
     }
 }
