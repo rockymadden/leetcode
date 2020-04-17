7335e84effd2c110e44dfc7b5391af8d6adca256
Rocky Madden <github@rockymadden.com>
2020-04-17 23:00:23 +0000

    feat: solve 283 with Java more simply

--- src/283.java
+++ src/283.java
@@ -1,32 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int a = 0;
-        int z = 0;
+        int j = 0;
         int tmp;
-
-        while (true) {
-            while (a < nums.length) {
-                if (nums[a] != 0) { break; }
-                else { a++; }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                j++;
             }
-            
-            if (a >= nums.length) { break; }
-            
-            while (z < nums.length) {
-                if (nums[z] == 0) { break; }
-                else { z++; }
-            }
-            
-            if (z >= nums.length) { break; }
-                        
-            if (z < a) {
-                tmp = nums[a];
-                nums[a] = nums[z];
-                nums[z++] = tmp;
-                tmp = a;
-            }
-            
-            a++;
         }
     }
 }
