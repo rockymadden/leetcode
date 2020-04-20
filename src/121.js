3d0719baf4787cd5e4b9682f2820762c4dc8b79a
Rocky Madden <github@rockymadden.com>
2020-04-20 00:13:46 +0000

    feat: solve 121 with Java

--- src/121.js
+++ src/121.js
@@ -5,14 +5,14 @@
 var maxProfit = function(P) {
     var minprice = Infinity;
     var maxprofit = 0;
-    
+
     for(var i = 0; i < P.length; i++) {
         if(P[i] < minprice) {
             minprice = P[i];
         } else if(P[i] - minprice > maxprofit) {
-           maxprofit = P[i] - minprice;
+            maxprofit = P[i] - minprice;
         }
     }
-    
+
     return maxprofit;
 };
