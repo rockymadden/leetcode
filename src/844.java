f240147b53305e3b3cb71b2801c364b9cebd7294
Rocky Madden <github@rockymadden.com>
2020-04-25 03:04:19 +0000

    feat: solve 844 with Java

---
+++ src/844.java
@@ -0,0 +1,46 @@
+// Optimal time-complexity O(n) and space-complexity O(1). However, there might be a way to reduce
+// memory usage further. 100% faster and 7% less (but the latter has been off lately).
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int s = S.length() - 1;
+        int t = T.length() - 1;
+        int sb = 0;
+        int tb = 0;
+
+        while (s >= 0 || t >= 0) {
+            while (s >= 0) {
+                if (S.charAt(s) == '#') {
+                    sb++;
+                    s--;
+                } else if (sb > 0) {
+                    sb--;
+                    s--;
+                } else {
+                    break;
+                }
+            }
+
+            while (t >= 0) {
+                if (T.charAt(t) == '#') {
+                    tb++;
+                    t--;
+                } else if (tb > 0) {
+                    tb--;
+                    t--;
+                } else {
+                    break;
+                }
+            }
+
+            if ((s >= 0 ? S.charAt(s) : "") != (t >= 0 ? T.charAt(t) : "")) {
+                return false;
+            }
+
+            s--;
+            t--;
+        }
+
+
+        return true;
+    }
+}
