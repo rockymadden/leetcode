7add77227b122b005e71ea57e185b784e1eba787
Rocky Madden <rockymadden@macbook.local>
2020-04-27 16:33:52 -0600

    feat: solve 525 via Map with Java

---
+++ src/525.map.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0, delta = 0;
+
+        map.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            delta += nums[i] == 0 ? -1 : 1;
+
+            if (map.containsKey(delta)) {
+                max = Math.max(max, i - map.get(delta));
+            } else {
+                map.put(delta, i);
+            }
+        }
+
+        return max;
+    }
+}
