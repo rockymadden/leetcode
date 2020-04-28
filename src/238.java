061dfbd715e41ccc4ddb2dda829b4ad02b22d2c7
Rocky Madden <rockymadden@macbook.local>
2020-04-28 07:40:55 -0600

    feat(238): resolve with solution

--- src/238.java
+++ src/238.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int[] L = new int[n];
+        int[] R = new int[n];
 
-        for (int i = 0, j = n - 1; i < n; i++, j--) {
-            if(i == 0) left[i] = nums[i];
-            else left[i] = left[i - 1] * nums[i];
+        L[0] = 1;
 
-            if(j == n - 1) right[j] = nums[j];
-            else right[j] = right[j + 1] * nums[j];
+        for (int i = 1; i < n; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        R[n - 1] = 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
         }
 
         for (int i = 0; i < n; i++) {
-            int l = i - 1 < 0 ? 1 : left[i - 1];
-            int r = i + 1 > n - 1 ? 1 : right[i + 1];
-
-            nums[i] = l * r;
+            nums[i] = L[i] * R[i];
         }
 
         return nums;
