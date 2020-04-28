814cbcbffa778f2044cc06258fbe8bbf24e28ccd
Rocky Madden <rockymadden@macbook.local>
2020-04-28 10:11:51 -0600

    feat(1295): solve optimal with Java

---
+++ src/1295.optimal.java
@@ -0,0 +1,14 @@
+// Leans on the constraints of the problem to remove the need to cast to String.
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] >= 10 && nums[i] <= 99) || (nums[i] >= 1000 && nums[i] <= 9999) || nums[i] == 100000) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
