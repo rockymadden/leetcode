c232f5f3adfb8729bdb8321d259d5d5a654f1a25
Rocky Madden <rockymadden@macbook.local>
2020-04-28 07:25:04 -0600

    refactor: minor formatting tweaks

--- src/238.java
+++ src/238.java
@@ -4,7 +4,7 @@ class Solution {
         int[] left = new int[n];
         int[] right = new int[n];
 
-        for(int i = 0, j = n - 1; i < n; i++, j--) {
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
             if(i == 0) left[i] = nums[i];
             else left[i] = left[i - 1] * nums[i];
 
@@ -12,7 +12,7 @@ class Solution {
             else right[j] = right[j + 1] * nums[j];
         }
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int l = i - 1 < 0 ? 1 : left[i - 1];
             int r = i + 1 > n - 1 ? 1 : right[i + 1];
 
