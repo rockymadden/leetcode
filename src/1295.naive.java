d660c5914a8c54f1b2cd9fde7993f0852486f304
Rocky Madden <rockymadden@macbook.local>
2020-04-28 10:08:11 -0600

    feat(1295): solve via naive with Java

---
+++ src/1295.naive.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (Integer.toString(nums[i]).length() % 2 == 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
