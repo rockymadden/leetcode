d8c036ecdc2744ac0de718c3789de69d21a8b6f1
Rocky Madden <git@rockymadden.com>
2020-05-03 17:15:44 -0600

    feat(643): solve with fewer double casts

--- src/643.java
+++ src/643.java
@@ -1,17 +1,17 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double max = Double.NEGATIVE_INFINITY;
+        int max = Integer.MIN_VALUE;
 
         for (int end = 0, start = 0, sum = 0; end < nums.length; end++) {
             sum += nums[end];
 
             if (end >= k - 1) {
-                max = Math.max(max, (double) sum / k);
+                max = Math.max(max, sum);
                 sum -= nums[start];
                 start++;
             }
         }
 
-        return max;
+        return (double) max / k;
     }
 }
