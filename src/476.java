8a8df1bb5e7effdc944d483262e0b5f3d9929ae8
Rocky Madden <git@rockymadden.com>
2020-05-04 07:55:25 -0600

    feat(476): solve with Java

---
+++ src/476.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findComplement(int num) {
+        int highest = (int) (Math.log(Integer.highestOneBit(num)) / Math.log(2));
+        int ans = 0;
+
+        for (int i = 0; i < highest; i++) {
+            if ((num & 1) == 0) ans |= (int) Math.pow(2, i);
+            num >>= 1;
+        }
+
+        return ans;
+    }
+}
