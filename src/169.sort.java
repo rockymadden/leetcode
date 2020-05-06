d17c0b4bf513a4bb5b77fde0b4dc2083fb4234b0
Rocky Madden <git@rockymadden.com>
2020-05-06 07:42:40 -0600

    feat(169): solve via sort with Java

---
+++ src/169.sort.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ref = Integer.MIN_VALUE;
+        int cnt = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != ref) cnt = 0;
+            if (++cnt > (nums.length / 2)) return nums[i];
+            ref = nums[i];
+        }
+
+        return -1;
+    }
+}
