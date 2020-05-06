42386275f56e11d7c9c20a2fc67dc3cddf3e9255
Rocky Madden <git@rockymadden.com>
2020-05-06 13:43:23 -0600

    refactor(904): minor nits

--- src/904.java
+++ src/904.java
@@ -1,13 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int totalFruit(int[] tree) {
         Map<Integer, Integer> baskets = new HashMap<>();
-        int start = 0, max = 0;
+        int max = 0;
 
-        for (int end = 0; end < tree.length; end++) {
+        for (int end = 0, start = 0; end < tree.length; end++) {
             int endFruit = tree[end];
             baskets.put(endFruit, baskets.getOrDefault(endFruit, 0) + 1);
 
-            while (start < end && baskets.size() > 2) {
+            while (baskets.size() > 2) {
                 int startFruit = tree[start];
                 baskets.put(startFruit, baskets.get(startFruit) - 1);
                 if (baskets.get(startFruit) < 1) baskets.remove(startFruit);
