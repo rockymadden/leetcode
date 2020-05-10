424fb279794aa78ebf82e612d7a12f5fe3af90e7
Rocky Madden <git@rockymadden.com>
2020-05-10 12:36:20 -0600

    feat(997): solve optimally with Java

---
+++ src/997.optimal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int judge = -10000 * (N - 1);
+        int[] t = new int[N + 1];
+
+        for (int i = 0; i < trust.length; i++) {
+            t[trust[i][0]]++;
+            t[trust[i][1]] -= 10000;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            if (t[i] == judge) return i;
+        }
+
+        return -1;
+    }
+}
