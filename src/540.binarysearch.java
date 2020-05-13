7ac35d590f4ea46eabc8eb1aa9cd04a697d527e1
Rocky Madden <git@rockymadden.com>
2020-05-12 19:56:49 -0600

    feat(540): solve via binary search with Java

---
+++ src/540.binarysearch.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            boolean even = (hi - mid) % 2 == 0;
+
+            if (nums[mid + 1] == nums[mid]) {
+                if (even) lo = mid + 2;
+                else hi = mid - 1;
+            } else if (nums[mid - 1] == nums[mid]) {
+                if (even) hi = mid - 2;
+                else lo = mid + 1;
+            } else {
+                return nums[mid];
+            }
+        }
+
+        return nums[lo];
+    }
+}
