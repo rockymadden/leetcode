1ff1a46e06c2534aa62b12b327026176fbcc8d8f
Rocky Madden <git@rockymadden.com>
2020-05-17 13:58:21 -0600

    feat(442): solve via sort with Java

---
+++ src/442.sort.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int prev = 0;
+
+        Arrays.sort(nums);
+
+        for (int num : nums) {
+            if (num == prev) list.add(num);
+            prev = num;
+        }
+
+        return list;
+    }
+}
