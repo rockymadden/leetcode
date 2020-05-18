213beccb17826fae626c20ec2be846ad6f3699f3
Rocky Madden <git@rockymadden.com>
2020-05-17 19:45:21 -0600

    feat(424): solve via sliding window and HashMap with Java

---
+++ src/424.sliding-window-hash-map.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> freq = new HashMap<>();
+        int longest = 0;
+        int repeat = 0;
+
+        for (int end = 0, start = 0; end < s.length(); end++) {
+            char endChar = s.charAt(end);
+            freq.put(endChar, freq.getOrDefault(endChar, 0) + 1);
+            repeat = Math.max(repeat, freq.get(endChar));
+
+            while (end - start + 1 - repeat > k) {
+                char startChar = s.charAt(start);
+                freq.put(startChar, freq.get(startChar) - 1);
+                start++;
+            }
+
+            longest = Math.max(longest, end - start + 1);
+        }
+
+        return longest;
+    }
+}
