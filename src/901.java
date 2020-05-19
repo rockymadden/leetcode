e2fcf5e54aad36ebc0c7beb2ced4d4f5872bae04
Rocky Madden <git@rockymadden.com>
2020-05-19 15:22:33 -0600

    feat(901): solve with Java

---
+++ src/901.java
@@ -0,0 +1,35 @@
+class StockSpanner {
+    private Deque<Pair> stack;
+
+    private class Pair {
+        public int price;
+        public int weight;
+
+        public Pair(int price, int weight) {
+            this.price = price;
+            this.weight = weight;
+        }
+    }
+
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+
+    public int next(int price) {
+        int weight = 1;
+
+        while (!stack.isEmpty() && stack.peek().price <= price) {
+            weight += stack.pop().weight;
+        }
+
+        stack.push(new Pair(price, weight));
+
+        return weight;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
