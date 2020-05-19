8c89c2975f1556469af158251f439bee466d809c
Rocky Madden <git@rockymadden.com>
2020-05-19 17:35:44 -0600

    feat(1394): solve with Java

---
+++ src/1394.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] freq = new int[501];
+        int largest = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            freq[arr[i]]++;
+            if (freq[arr[i]] == arr[i]) largest = Math.max(largest, arr[i]);
+        }
+
+        for (int i = largest; i >= 1; i--) {
+            if (freq[i] == i) return i;
+        }
+
+        return -1;
+    }
+}
