5e6da1574d10c03089389b1868be7689da949168
Rocky Madden <git@rockymadden.com>
2020-05-19 18:00:16 -0600

    feat(1403): solve with Java

---
+++ src/1403.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int sum = 0;
+        Arrays.sort(nums);
+
+        for (int num : nums) sum += num;
+
+        for (int i = nums.length - 1, gt = 0; i >= 0; i--) {
+            ans.add(nums[i]);
+            gt += nums[i];
+            sum -= nums[i];
+            if (gt > sum) break;
+        }
+
+        return ans;
+    }
+}
