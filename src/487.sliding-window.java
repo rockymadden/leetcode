e08b4a7b369e7246ea40a3b9f8db21438ae193f9
Rocky Madden <git@rockymadden.com>
2020-05-20 20:08:35 -0600

    feat(487): minor speed increase with Java

--- src/487.sliding-window.java
+++ src/487.sliding-window.java
@@ -1,13 +1,12 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int[] freq = new int[2];
         int ans = 0;
 
-        for (int end = 0, start = 0; end < nums.length; end++) {
-            freq[nums[end]]++;
+        for (int end = 0, start = 0, zeros = 0; end < nums.length; end++) {
+            if (nums[end] == 0) zeros++;
 
-            while (start < end && freq[0] > 1) {
-                freq[nums[start]]--;
+            while (start < end && zeros > 1) {
+                if (nums[start] == 0) zeros--;
                 start++;
             }
 
