4d7397730b03de6f80612035b669cb4da5f45c52
Rocky Madden <git@rockymadden.com>
2020-05-24 16:40:19 -0600

    refactor(1198): shorter

--- src/1198.count.java
+++ src/1198.count.java
@@ -4,11 +4,11 @@ class Solution {
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                freq[mat[i][j]]++;
+                 if (++freq[mat[i][j]] == mat.length) {
+                     return mat[i][j];
+                 }
             }
         }
-
-        for (int i = 0; i < 10001; i++) if (freq[i] == mat.length) return i;
 
         return -1;
     }
