4379aa6ed948a1c376bc4abf6105c04c0d880767
Rocky Madden <rockymadden@macbook.lan>
2020-06-04 06:00:54 -0600

    feat(344): solve via two pointers with Java

---
+++ src/main/java/Solution344ViaTwoPointers.java
@@ -0,0 +1,11 @@
+public class Solution344ViaTwoPointers {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+
+        while (left < right) {
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+        }
+    }
+}
