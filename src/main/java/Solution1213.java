c886d2eed3239968c22ac4873cc57aee01d06983
Rocky Madden <git@rockymadden.com>
2020-06-10 20:24:00 -0600

    feat(1213): solve with Java

---
+++ src/main/java/Solution1213.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+ * Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, return a
+ * sorted array of only the integers that appeared in all three arrays.
+ *
+ * Time-complexity: O(n)
+ * Space-complexity: O(1)
+ */
+public class Solution1213 {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> ans = new ArrayList<>();
+        int a = 0, b = 0, c = 0;
+
+        while (a < arr1.length) {
+            while (b < arr2.length - 1 && arr2[b] < arr1[a]) b++;
+            while (c < arr3.length - 1 && arr3[c] < arr1[a]) c++;
+            if (arr1[a] == arr2[b] && arr1[a] == arr3[c]) ans.add(arr1[a]);
+            a++;
+        }
+
+        return ans;
+    }
+}
