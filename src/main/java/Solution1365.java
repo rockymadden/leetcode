cf15531891f080872e1923324901246bf25df887
Rocky Madden <git@rockymadden.com>
2020-06-13 13:57:47 -0600

    refactor(1365): resolve with Java

--- src/main/java/Solution1365.java
+++ src/main/java/Solution1365.java
@@ -1,15 +1,27 @@
+/**
+ * Given the array nums, for each nums[i] find out how many numbers in the array are smaller than
+ * it. That is, for each nums[i] you have to count the number of valid j's such that j != i and
+ * nums[j] < nums[i].
+ *
+ * Return the answer in an array.
+ */
 public class Solution1365 {
+    /**
+     * Time-complexity: O(n)
+     * Space-complexity: O(1)
+     */
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] freq = new int[101];
+        int[] frq = new int[101];
 
-        for (int num : nums) freq[num]++;
-        for (int i = 0, cnt = 0; i < 101; i++) {
-            if (freq[i] > 0) {
-                cnt += freq[i];
-                freq[i] = cnt - freq[i];
-            }
+        for (int num : nums) frq[num]++;
+        for (int i = 0, count = 0; i < 101; i++) {
+            int tmp = frq[i];
+            frq[i] = count;
+            count += tmp;
         }
-        for (int i = 0; i < nums.length; i++) nums[i] = freq[nums[i]];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = frq[nums[i]];
+        }
 
         return nums;
     }
