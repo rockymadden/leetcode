36dd9b7b0567de763d76961b4dd4c409401950ec
Rocky Madden <git@rockymadden.com>
2020-06-13 15:52:26 -0600

    refactor(704): resolve with Java

--- src/main/java/Solution704.java
+++ src/main/java/Solution704.java
@@ -1,20 +1,24 @@
+/**
+ * Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search
+ * target in nums. If target exists, then return its index, otherwise return -1.
+ */
 public class Solution704 {
+    /**
+     * Time-complexity: O(log n)
+     * Space-complexity: O(1)
+     */
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        int lo = 0;
+        int hi = nums.length - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
 
-            if (nums[mid] < target) {
-                low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
-            } else {
-                return mid;
-            }
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
         }
 
-        return -1;
+        return nums[lo] == target ? lo : -1;
     }
 }
