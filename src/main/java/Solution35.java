4c7cde60faf9b5c9516d9d0d0b39b3a7e9122739
Rocky Madden <git@rockymadden.com>
2020-06-15 16:49:44 -0600

    feat(35): solve with Java

---
+++ src/main/java/Solution35.java
@@ -0,0 +1,15 @@
+public class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return nums[low] < target ? low + 1 : low;
+    }
+}
