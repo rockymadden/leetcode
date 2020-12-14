b151fddbcbceec50bcbe22c745393e48b81f1dce
Rocky Madden <git@rockymadden.com>
2020-12-14 01:56:07 +0000

    refactor(1480): resolve with Java

--- src/main/java/Solution1480.java
+++ src/main/java/Solution1480.java
@@ -1,8 +1,8 @@
 public class Solution1480 {
     public int[] runningSum(int[] nums) {
-        for (int i = 0, sum = 0; i < nums.length; i++) {
-            sum += nums[i];
-            nums[i] = sum;
+        for (int i = 0, p = 0; i < nums.length; i++) {
+            nums[i] = nums[i] + p;
+            p = nums[i];
         }
 
         return nums;
