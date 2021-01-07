a9b190ae13ead59f718533cff3ee7e1be7064f13
Rocky Madden <git@rockymadden.com>
2021-01-07 13:09:13 -0700

    feat(27): resolve with Java

--- src/main/java/Solution27.java
+++ src/main/java/Solution27.java
@@ -1,14 +1,11 @@
 public class Solution27 {
     public int removeElement(int[] nums, int val) {
-        int j = 0;
+        int i = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[j] = nums[i];
-                j++;
-            }
+        for (int non = 0; non < nums.length; non++) {
+            if (nums[non] != val) nums[i++] = nums[non];
         }
 
-        return j;
+        return i;
     }
 }
